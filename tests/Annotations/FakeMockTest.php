6e27d13732a3e06031cc79acd9b7f65f7cfcdce3
ppawliczuk <przemyslaw@pawliczuk.pl>
2018-11-08 13:37:21 +0100

    main annotation covered by tests

---
+++ tests/Annotations/FakeMockTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Tests\Er1z\FakeMock\Annotations;
+
+
+use Er1z\FakeMock\Annotations\FakeMock;
+use PHPUnit\Framework\TestCase;
+
+class FakeMockTest extends TestCase
+{
+
+    public function testInstantiationWithParams()
+    {
+        $data = [
+            'useAsserts'=>false
+        ];
+
+        $annotation = new FakeMock($data);
+        $this->assertFalse($annotation->useAsserts);
+    }
+
+
+}
